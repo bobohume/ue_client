e23f5aa2ea8c726fe3a983f6a16340333f1a0025
bobohume <754266963@qq.com>
2019-09-17 18:28:36 +0800

    111

--- Source/TEST2/Entity.cpp
+++ Source/TEST2/Entity.cpp
@@ -16,4 +16,25 @@ Entity::Entity()
 	};
 
 	REGISTER_PACKET(new W_C_ENTITY(), _W_C_ENTITY);
+	//REGISTER_PACKET(new W_C_ENTITY(), std::bind(&Entity::_W_C_ENTITY2, this, std::placeholders::_1));
+}
+
+bool Entity::_W_C_ENTITY3(::google::protobuf::Message* _packet) {
+	auto packet = (W_C_ENTITY*)(_packet);
+	if (!packet) {
+		return false;
+	}
+
+
+	return true;
+}
+
+bool Entity::_W_C_ENTITY2(::google::protobuf::Message* _packet) {
+	auto packet = (W_C_ENTITY*)(_packet);
+	if (!packet) {
+		return false;
+	}
+
+
+	return true;
 }
--- Source/TEST2/Entity.h
+++ Source/TEST2/Entity.h
@@ -1,8 +1,12 @@
 #pragma once
+#include "message/Packet.h"
 class Entity
 {
+public:
 	Entity();
 	~Entity() {};
+	static bool _W_C_ENTITY3(::google::protobuf::Message* _packet);
+	bool _W_C_ENTITY2(::google::protobuf::Message* _packet);
 public:
 	static Entity* Instance() {
 		static Entity s_Instace;
