8d2f1a55be905927162cee78963367f735db6eb3
bobohume <754266963@qq.com>
2019-09-20 13:20:11 +0800

    111

--- Source/TEST2/TEST2.Build.cs
+++ Source/TEST2/TEST2.Build.cs
@@ -8,7 +8,7 @@ public class TEST2 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core",  "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Navmesh", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core",  "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay"});
         bEnableShadowVariableWarnings = false;
         bEnableUndefinedIdentifierWarnings = false;
         bEnableExceptions = true;
